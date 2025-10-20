bce86a6bb9b41e5616db1f5ce91a77cde2d4c084
martafronton <martafronton@gmail.com>
2025-10-20 19:40:34 +0200

    fix: registro de actividad
    Corrige validación de campos opcionales en el formulario

--- desafio-swing/src/formulario.js
+++ desafio-swing/src/formulario.js
@@ -3,13 +3,32 @@ export function initFormulario() {
     const claseOpciones = document.getElementById('clase-opciones');
     const actividadOpciones = document.getElementById('actividad-opciones');
   
+    claseOpciones.classList.remove('show');
+    actividadOpciones.classList.remove('show');
+
     tipoSelect.addEventListener('change', () => {
+      claseOpciones.querySelectorAll('[required]').forEach(campo => campo.required = false);
+      actividadOpciones.querySelectorAll('[required]').forEach(campo => campo.required = false);
+  
       if (tipoSelect.value === 'clase') {
         claseOpciones.classList.add('show');
         actividadOpciones.classList.remove('show');
-      } else {
+        claseOpciones.querySelectorAll('input, select, textarea')
+          .forEach(campo => campo.required = true);
+  
+      } else if (tipoSelect.value === 'actividad') {
         claseOpciones.classList.remove('show');
         actividadOpciones.classList.add('show');
+        actividadOpciones.querySelectorAll('input:not(#profesores2), select, textarea:not(#descripcion)') 
+          .forEach(campo => campo.required = true);
+                    profesores = document.getElementById("profesores2")
+        profesores.required = false;
+        descripcion = document.getElementById("descripcion")
+        descripcion.required = false;
+  
+      } else {
+        claseOpciones.classList.remove('show')
+        actividadOpciones.classList.remove('show');
       }
     });
   }
